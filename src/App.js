3bea054dde3dd765bc6f3485e756ef076d30adb7
filipa-pf <anafilipa.paesdefaria@gmail.com>
2021-07-07 20:41:35 +0100

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       Weather
       <footer>
       <a href="https://github.com/filipa-pf/finalProjectSheCodesPlus" target="_blank" rel="noreferrer" id="git">GitHub access here!</a>
       </footer>
+      </div>
     </div>
   );
 }
