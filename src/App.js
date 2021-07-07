9b4a5edac11e0a95b3f035f7c8385fcdc1e0f213
filipa-pf <anafilipa.paesdefaria@gmail.com>
2021-07-07 19:47:45 +0100

    Added link to GitHub

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ export default function App() {
   return (
     <div className="App">
       Weather
+      <footer>
+      <a href="https://github.com/filipa-pf/finalProjectSheCodesPlus" target="_blank" rel="noreferrer" id="git">GitHub access here!</a>
+      </footer>
     </div>
   );
 }
